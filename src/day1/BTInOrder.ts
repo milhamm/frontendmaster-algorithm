86c9ab0d47398019a5e206eb42c9db1756e69781
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-03-09 01:01:58 +0700

    finish: BTInOrder

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,1 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {}
+const dfs = (head: BinaryNode<number> | null, arr: number[]) => {
+    if (!head) {
+        return;
+    }
+
+    dfs(head.left, arr);
+    arr.push(head.value);
+    dfs(head.right, arr);
+};
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    const arr: number[] = [];
+    dfs(head, arr);
+    return arr;
+}
