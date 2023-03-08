32507682e41738d7e40e05167a95bd2bc4b27dab
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-03-09 01:00:03 +0700

    finish: BTPreOrder

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,15 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+const dfs = (head: BinaryNode<number> | null, arr: number[]) => {
+    if (!head) {
+        return;
+    }
 
+    arr.push(head.value);
+    dfs(head.left, arr);
+    dfs(head.right, arr);
+};
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const arr: number[] = [];
+    dfs(head, arr);
+    return arr;
 }
